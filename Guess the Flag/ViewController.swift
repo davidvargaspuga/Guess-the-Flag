4dca98f50c9d2390b7052acf59e6b12970945ca8
davidvargaspuga <david.vargas18@yahoo.com>
2020-07-18 21:58:03 -0700

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by David Vargaspuga on 7/18/20.
+//  Copyright © 2020 David Vargas Puga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
